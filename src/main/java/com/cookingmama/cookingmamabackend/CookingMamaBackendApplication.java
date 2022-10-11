47c7f8b4f868c347cd271227da1794ded67c6652
Hussein Isron <47143174+isron89@users.noreply.github.com>
2022-10-11 16:01:40 +0700

    backend cooking mana init

---
+++ src/main/java/com/cookingmama/cookingmamabackend/CookingMamaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cookingmama.cookingmamabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingMamaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingMamaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cookingmama/cookingmamabackend/CookingMamaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookingmama.cookingmamabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingMamaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
